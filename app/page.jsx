16899d623dd2de439266b489b2ae3e651e893281
saman karimi <sam.karimi727@gmail.com>
2026-08-10 02:18:52 +0330

    update 5 files and create 4 files

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import ThemeButton from "@/components/UI/toggleTheme";
 
 
 export default function Home() {
   return (
 <div>
-  
+  <ThemeButton/>
 </div>
   );
 }
---
+++ components/UI/toggleTheme.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { useTheme } from "@teispace/next-themes";
+import Image from "next/image";
+
+ function ThemeButton() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className={`min-w-12.5 h-7.5 relative flex-between rounded-[50px] cursor-pointer px-1 transition-all ${
+        theme === "light" ? "bg-sky-200" : "bg-blue-600"
+      }`}
+    >
+      <Image src="/icons/sun.png" alt="sun" width={14} height={14} />
+      <div
+        className={`w-6.25 h-6.25 rounded-full absolute transition-all  bg-white ${
+          theme === "light" ? "left-px" : "right-px"
+        }`}
+      >
+        {theme === "light" ? (
+          <img
+            src="/icons/sun.png"
+            alt="logo-image"
+            className="w-25 h-25 rounded-full object-cover"
+          />
+        ) : (
+          <img
+            src="/icons/newmoon.png"
+            alt="logo-image"
+            className="w-25 h-25 rounded-full object-cover"
+          />
+        )}
+      </div>
+      <Image src="/icons/moon.png" alt="moon" width={14} height={14} />
+    </div>
+  );
+}
+
+export default ThemeButton;;
