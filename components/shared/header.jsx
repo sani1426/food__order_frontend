0eea9ae015bf5d29a93bf746e8cc28e5f6ae1cc1
saman karimi <sam.karimi727@gmail.com>
2026-08-11 03:29:13 +0330

    update header.jsx

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -1,3 +1,4 @@
+import { navItems } from "@/common/data";
 import Link from "next/link";
 
 const Header = () => {
@@ -6,12 +7,22 @@ const Header = () => {
       <Link href="/" className="text-gradient font-semibold text-2xl">
         پیتزاویچ
       </Link>
-      <nav className="flex items-center gap-8 text-dark-muted font-semibold">
-        <Link href="/">صفحه اصلی</Link>
-        <Link href="/menu">منو</Link>
-        <Link href="/about">درباره ما</Link>
-        <Link href="/contact">تماس با ما</Link>
-        <Link className="bg-prime text-white rounded-full px-8 py-2 py-2" href="sign-up">ثبت نام</Link>
+      <nav className="flex items-center gap-8 text-light-muted font-semibold">
+        {navItems.map((link) => (
+          <Link
+            className="hover:text-second transition-colors duration-300"
+            key={link.href}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        ))}
+        <Link
+          className="bg-prime text-white rounded-full px-8 py-2"
+          href="sign-up"
+        >
+          ثبت نام
+        </Link>
       </nav>
     </header>
   );
