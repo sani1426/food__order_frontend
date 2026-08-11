a6fdfded75e7a146391ee74a628e93fe83dba45f
saman karimi <sam.karimi727@gmail.com>
2026-08-12 02:47:52 +0330

    update header.jsx

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -4,11 +4,11 @@ import ThemeButton from "../UI/toggleTheme";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between w-6xl max-w-[90vw] mx-auto bg-backlight-second dark:bg-backdark-second">
-      <Link href="/" className="text-gradient font-semibold text-2xl">
+    <header className="flex items-center justify-between w-6xl max-w-[90vw] mx-auto bg-backlight-second dark:bg-backdark-second p-5 rounded-[40px]">
+      <Link href="/" className="text-gradient font-semibold text-3xl md:text-4xl">
         پیتزاویچ
       </Link>
-      <nav className="flex items-center gap-8 text-light-muted font-semibold">
+      <nav className="hidden md:flex items-center gap-8 text-light-muted font-semibold ">
         {navItems.map((link) => (
           <Link
             className="hover:text-second transition-colors duration-300"
@@ -18,14 +18,16 @@ const Header = () => {
             {link.name}
           </Link>
         ))}
+      </nav>
+      <div className="flex items-center justify-center gap-5">
         <Link
           className="bg-prime text-white rounded-full px-8 py-2"
           href="sign-up"
         >
           ثبت نام
         </Link>
-      </nav>
-      <ThemeButton />
+        <ThemeButton />
+      </div>
     </header>
   );
 };
