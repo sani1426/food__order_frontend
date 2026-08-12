1809904ef1ab37090b73c870dad57ee58d765671
saman karimi <sam.karimi727@gmail.com>
2026-08-12 03:34:46 +0330

    update globals.css and Hero.jsx

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -3,12 +3,25 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <section className="">
-        <h1>همه ی مشکلات با یه پیتزا خوب حل میشن</h1>
-        <p>پیتزا تکه ی گمشده است که روزها رو کامل میکنه , یه راه حل ساده و خوشمزه</p>
-        <Image />
+    <section className="grid grid-cols-2 ">
+      <div className="">
+        <h1 className="text-4xl font-semibold gradient-anime">
+          همه ی مشکلات با یه پیتزا خوب حل میشن
+        </h1>
+        <p className="mt-4 text-light-muted">
+          پیتزا تکه ی گمشده است که روزها رو کامل میکنه , یه راه حل ساده و خوشمزه
+        </p>
+      </div>
+      <div className="w-16 h-16 relative">
+        <Image
+          src={"/images/hero.png"}
+          alt="hero-image/pizza"
+          layout={"fill"}
+          objectFit={"contain"}
+        />
+      </div>
     </section>
-  )
+  );
 }
 
 export default Hero
