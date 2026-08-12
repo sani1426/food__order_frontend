309b9cd7b854557c900369d102236d944b2aa6ff
saman karimi <sam.karimi727@gmail.com>
2026-08-12 03:39:48 +0330

    update Hero.jsx

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -5,14 +5,20 @@ const Hero = () => {
   return (
     <section className="grid grid-cols-2 ">
       <div className="">
-        <h1 className="text-4xl font-semibold gradient-anime">
+        <h1 className="text-4xl font-semibold text-gradient gradient-anime">
           همه ی مشکلات با یه پیتزا خوب حل میشن
         </h1>
-        <p className="mt-4 text-light-muted">
+        <p className="my-4 text-light-muted">
           پیتزا تکه ی گمشده است که روزها رو کامل میکنه , یه راه حل ساده و خوشمزه
         </p>
+        <div className="">
+          <button className="bg-prime text-white px-8 py-2 rounded-full">
+            سفارش
+          </button>
+          <button></button>
+        </div>
       </div>
-      <div className="w-16 h-16 relative">
+      <div className=" relative">
         <Image
           src={"/images/hero.png"}
           alt="hero-image/pizza"
