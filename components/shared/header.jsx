6f7f4986975ccbf5a4d857f271bcb56b1992e5d8
saman karimi <sam.karimi727@gmail.com>
2026-08-12 04:07:09 +0330

    update header.jsx

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -5,29 +5,34 @@ import {} from "react-icons"
 
 const Header = () => {
   return (
-    <header className=" fixed flex mt-4 items-center justify-between w-6xl max-w-[90vw] mx-auto bg-sec p-5 rounded-[40px]">
-      <Link href="/" className="text-gradient font-semibold text-3xl md:text-4xl">
-        پیتزاویچ
-      </Link>
-      <nav className="hidden md:flex items-center gap-8 text-light-muted font-semibold ">
-        {navItems.map((link) => (
-          <Link
-            className="hover:text-second transition-colors duration-300"
-            key={link.href}
-            href={link.href}
-          >
-            {link.name}
-          </Link>
-        ))}
-      </nav>
-      <div className="flex items-center justify-center gap-5">
+    <header className="fixed w-full flex justify-center items-center">
+      <div className=" flex mt-4 items-center justify-between w-6xl max-w-[90vw] mx-auto bg-sec p-5 rounded-[40px] z-100">
         <Link
-          className="bg-prime text-white rounded-full px-8 py-2"
-          href="sign-up"
+          href="/"
+          className="text-gradient font-semibold text-3xl md:text-4xl"
         >
-          ثبت نام
+          پیتزاویچ
         </Link>
-        <ThemeButton />
+        <nav className="hidden md:flex items-center gap-8 text-light-muted font-semibold ">
+          {navItems.map((link) => (
+            <Link
+              className="hover:text-second transition-colors duration-300"
+              key={link.href}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+        <div className="flex items-center justify-center gap-5">
+          <Link
+            className="bg-prime text-white rounded-full px-8 py-2"
+            href="sign-up"
+          >
+            ثبت نام
+          </Link>
+          <ThemeButton />
+        </div>
       </div>
     </header>
   );
