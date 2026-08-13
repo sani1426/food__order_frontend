0bc91663d4619973cd300e04e91d040d26fedce3
saman karimi <sam.karimi727@gmail.com>
2026-08-13 04:03:35 +0330

    update animationTitle.jsx

--- components/animation/animationTitle.jsx
+++ components/animation/animationTitle.jsx
@@ -1,18 +1,23 @@
 "use client"
 
 import {gsap} from "@/lib/gsap";
+import { useGSAP } from "@gsap/react";
 
 const Title = ({title}) => {
-    const tl = gsap.timeline();
-    tl.to(".text", { opacity: 1, duration: 0.1 })
-      .to(".text", { opacity: 0.2, duration: 0.05 })
-      .to(".text", { opacity: 1, duration: 0.1 })
-      .to(".text", { opacity: 0, duration: 0.05 })
-      .to(".text", {
-        opacity: 1,
-        textShadow: "0 0 20px #00FF66",
-        duration: 0.3,
-      });
+    useGSAP(() => {
+  const tl = gsap.timeline();
+  tl.set(".text", { opacity: 0 })
+  .to(".text", { opacity: 1, duration: 0.1 })
+    .to(".text", { opacity: 0.2, duration: 0.05 })
+    .to(".text", { opacity: 1, duration: 0.1 })
+    .to(".text", { opacity: 0, duration: 0.05 })
+    .to(".text", {
+      opacity: 1,
+      textShadow: "0 0 20px #00FF66",
+      duration: 0.3,
+    });
+    })
+  
   return (
     <h1 className="text">
       {title}
