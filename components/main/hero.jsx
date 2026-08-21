e2043fce69ef95debf3954fb0aecc583dce4af72
saman karimi <sam.karimi727@gmail.com>
2026-08-21 03:45:07 +0330

    update hero.jsx

--- components/main/hero.jsx
+++ components/main/hero.jsx
@@ -13,20 +13,27 @@ const Hero = () => {
         className="w-full h-full object-cover z-[-1]"
       />
       <div className="absolute inset-0 bg-linear-to-r from-[rgba(0,0,0,0.3)] to-[rgba(0,0,0,0.2)] z-10 flex items-center justify-center ">
-        <div className="flex-col w-full text-white p-6 gap-y-4 ">
-          <h1 className="text-3xl md:text-7xl  text-gradient gradient-anime font-semibold">
+        <div className="flex-col w-full text-white p-6  ">
+          <h1 className="text-3xl md:text-7xl  text-gradient gradient-anime font-semibold mb-4">
             یه انتخاب خوشمزه و لذیذ"
           </h1>
-          <h2 className="text-lg md:text-2xl text-gradient gradient-anime font-semibold">
+          <h2 className="text-xl md:text-3xl text-gradient gradient-anime font-semibold mb-4">
             تکه ای از روز که بهترین طعم هارو داره
           </h2>
-
-          <Link
-            href="#"
-            className=" px-6 py-2 rounded-md text-lg bg-transparent border border-prime text-prime hover:bg-prime hover:text-white transition-all duration-200 "
-          >
-            مشاهده منو
-          </Link>
+          <div className="flex justify-between items-center gap-4">
+            <Link
+              href="#"
+              className=" px-6 py-2 rounded-md text-lg bg-transparent border border-prime text-prime hover:bg-prime hover:text-white transition-all duration-200 "
+            >
+              سفارش آنلاین
+            </Link>
+            <Link
+              href="#"
+              className=" px-6 py-2 rounded-md text-lg bg-prime border border-prime text-white hover:bg-transparent hover:text-prime transition-all duration-200 "
+            >
+              مشاهده منو
+            </Link>
+          </div>
         </div>
       </div>
     </section>
