ea865a6d7864b2d02d155651a229dd41fba3c7d8
saman karimi <sam.karimi727@gmail.com>
2026-08-21 04:09:45 +0330

    update hero.jsx

--- components/main/hero.jsx
+++ components/main/hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       />
       <div className="absolute inset-0 bg-linear-to-r from-[rgba(0,0,0,0.3)] to-[rgba(0,0,0,0.2)] z-10 flex items-center justify-center ">
         <div className="flex-col w-full text-white p-2 lg:p-6  ">
-          <h1 className="text-3xl lg:text-7xl  text-gradient gradient-anime font-semibold mb-4 md:mb-8">
+          <h1 className="text-3xl lg:text-7xl  text-gradient gradient-anime font-semibold mb-4 md:mb-8 text-center md:text-right">
             یه انتخاب خوشمزه و لذیذ"
           </h1>
           <h2 className=" text-xl lg:text-3xl text-gradient gradient-anime font-semibold mb-12">
