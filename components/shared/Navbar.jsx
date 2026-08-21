98486804fc577a9642f2ff508c4f3549b740a033
saman karimi <sam.karimi727@gmail.com>
2026-08-22 02:36:46 +0330

    update Navbar.jsx

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -9,7 +9,7 @@ import {
   MobileNavHeader,
   MobileNavToggle,
   MobileNavMenu,
-} from "@/components/ui/resizable-navbar";
+} from "@/components/UI/resizable-navbar";
 import { useState } from "react";
 
 export default function Navbar() {
