3b706db75680156395ef9a37cfec3bc32c1574c6
saman karimi <sam.karimi727@gmail.com>
2026-08-21 04:54:33 +0330

    update HomeMenu.jsx

--- components/main/HomeMenu.jsx
+++ components/main/HomeMenu.jsx
@@ -3,22 +3,24 @@ import React from 'react'
 
 const HomeMenu = () => {
   return (
-    <section className="py-8 ">
-      <div className="text-center md:-7xl max-w-[90vw]">
-        {/* <div className="h-48 relative">
+    <section className="py-8 bg-sec ">
+      <div className="md:-7xl max-w-[90vw]">
+        <div className="text-center ">
+          {/* <div className="h-48 relative">
             <Image src={""} alt='salad' layout='fill' objectFit='contain' />
         </div> */}
-        <h3 className="text-gray-600 font-semibold text-2xl">مشاهده کنید</h3>
-        <h2 className="text-prime font-bold text-5xl ">منو</h2>
-      </div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-gray-300 p-4 rounded-md">
-          <img src="/images/pizza/pepperoni.jpg" alt="pepperoni pizza" />
-          <h4 className="">پیتزا پپرونی</h4>
-          <p className="">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و استفاده
-            طراحان گرافیک است
-          </p>
+          <h3 className="text-gray-600 font-semibold text-2xl">مشاهده کنید</h3>
+          <h2 className="text-prime font-bold text-5xl ">منو</h2>
+        </div>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="bg-gray-300 p-4 rounded-md">
+            <img src="/images/pizza/pepperoni.jpg" alt="pepperoni pizza" />
+            <h4 className="">پیتزا پپرونی</h4>
+            <p className="">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+              استفاده طراحان گرافیک است
+            </p>
+          </div>
         </div>
       </div>
     </section>
